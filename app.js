087536b3be9d371b29ff92ad0a09c8659b1477e9
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-14 05:27:47 +0100

    removed comments and left only core code

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const filesystem = require('fs');
+const yargs = require('yargs');
 const chalk = require('chalk');
 const notes = require('./notes.js');
 
-// filesystem.writeFileSync('notes.txt', 'File has been created and line written in by node\n My name is moh btw');
-// console.log(chalk.green.bold.inverse('File created!'));
+yargs.command({
+    'command':'add',
+    'describe':'Add a new note',
+    handler: function(){
+        console.log(chalk.green('New note added'));
+    }
+})
 
-// filesystem.appendFileSync('notes.txt', '\nThis line is being appended to the existing one in the .txt file');
-
-// let notesVar = notes();
-// // console.log(notesVar);
-// console.log(chalk.red.underline('Connection failed'));
-
-// console.log(chalk.green(process.argv[2]));
-console.log(process.argv);
-const command = process.argv[2];
-if(command === "add"){
-    console.log("Adding note");
-}else if(command === "delete"){
-    console.log("Deleting note");
-}else{
-    console.log(`Unrecognized command\nAvailable Commands are add & delete`);
-}
+console.log(yargs.argv);
