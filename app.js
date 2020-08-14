848bdf81d4c4c1a2ebe91b87c63e23e574b8fdcd
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-14 05:52:17 +0100

    app.js is looking nice :)

--- app.js
+++ app.js
@@ -2,13 +2,52 @@ const filesystem = require('fs');
 const yargs = require('yargs');
 const chalk = require('chalk');
 const notes = require('./notes.js');
+const { demandOption } = require('yargs');
 
 yargs.command({
     'command':'add',
-    'describe':'Add a new note',
-    handler: function(){
-        console.log(chalk.green('New note added'));
+    'describe':'Adds a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function(argv){
+        console.log(chalk.green('New note added'), argv);
     }
-})
+});
+
+yargs.command({
+    'command':'delete',
+    'describe':'Deletes an existing note',
+    builder: {
+        title: {
+            describe: 'Note title'
+        }
+    },
+    handler: function(argv){
+        console.log(chalk.green('Removed existing note'));
+    }
+});
+
+yargs.command({
+    'command':'read',
+    'describe':'Reads an existing note',
+    handler: function(){
+        console.log(chalk.green('Read an existing note'));
+    }
+});
+
+yargs.command({
+    'command':'list',
+    'describe':'Lists all existing notes',
+    handler: function(){
+        console.log(chalk.green('Listed all existing notes'));
+    }
+});
+
+yargs.parse();
 
 console.log(yargs.argv);
