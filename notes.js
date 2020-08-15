76bf503be1fe3e6a4cd2ca8c629d2908872ecef2
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-15 02:57:53 +0100

    feature: added delete note(with insighful feedback)

--- notes.js
+++ notes.js
@@ -40,7 +40,23 @@ const loadNotes = function(){
     
 }
 
+const deleteNote = function(title){
+    const notes = loadNotes();
+    const notesToKeep = notes.filter(function(note){
+        return note.title !== title;
+    });
+
+    
+    if(notes.length > notesToKeep.length){
+        console.log(chalk.green(`Note with title: ${title} has been deleted`));
+        saveNotes(notesToKeep);
+    }else{
+        console.log(chalk.red('No note removed'));
+    }
+}   
+
 module.exports = {
     notes: notes,
-    addNote: addNote
+    addNote: addNote,
+    deleteNote: deleteNote
 };
