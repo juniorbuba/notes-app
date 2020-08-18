0c4dd19332b6efb5ee4af2e943f757af031ef685
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-18 04:29:04 +0100

    chore: modified log to include social links

--- author.js
+++ author.js
@@ -1,7 +1,15 @@
 const chalk = require('chalk');
+const github = 'https://github.com/juniorbuba/notes-app';
+const twitter = 'https://twitter.com/web_walkerX';
 
 const aboutMoh = function(){
-    console.log('The name is Moh');
+    console.log(chalk.green('========================================='));
+    console.log(chalk.green('The name is Moh'));
+    console.log('I\'m just messing around with this package but if you like it, give a star on github');
+    console.log(github);
+    console.log(`Don\'t forget to follow me on twitter: ${twitter}`);
+    console.log(chalk.yellow('Thanks'));
+    console.log(chalk.green('========================================='));
 };
 
 module.exports = {aboutMoh:aboutMoh};
